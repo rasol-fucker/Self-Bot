a834b599a45ccde95d2e02b162cb953c891cc377
rasol_fucker <mohmadrsol81@gmail.com>
2016-06-15 13:34:18 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,11 +1,11 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "Self-Bot v2 \n by rasilfucker :) <3 \n @rasol_fucker_rasol"
 end 
 return {
   patterns = {
-    "^#version$",
+    "^#rasol$",
     "^#bot$",
     "^#selfbot$"
   },
